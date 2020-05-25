50d26c4feb18f50267793703ea9c1e4e9ec10be9
Steven Black <steveb@stevenblack.com>
2020-05-24 22:42:27 -0400

    Commit after some mucking around.

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,59 @@
 
 use std::option::Option;
 
+#[derive(Debug, Clone)]
 pub struct Hook<'a> {
     pub name: String,
+    pub description: String,
     pub hook: Option<Box<&'a Hook<'a>>>,
 }
 
+pub trait Hookable<T> {
+    fn preprocess(&self, t: T) -> bool;
+    fn process(&self, t: T) -> T;
+    fn execute(&self, t: T) -> T;
+    fn postprocess(&self, t: T) -> T;
+    fn sethook(&self, h: Hook);
+}
+
+
 impl Hook<'_> {
-    pub fn sethook(&mut self, hook: Option<Box<Hook>>) {
+    pub fn process(&self) {
+        println!("{}: {}", self.name, self.description);
         match &self.hook {
             Some(h) => {
-                h.sethook(hook);
-            },
+                h.process();
+            }
             None => {
-                self.hook = None;
-            },
+                println!("{}", "🏆");
+            }
         }
     }
+
+    // pub fn sethook(&mut self, hook: Option<Box<Hook>>) {
+    //     match &self.hook {
+    //         Some(h) => {
+    //             h.sethook(hook);
+    //         }
+    //         None => {
+    //             self.hook = None;
+    //         }
+    //     }
+    // }
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    let mut h1 = Hook {
+        name: "Hook1".to_string(),
+        description: "First hook".to_string(),
+        hook: None,
+    };
+    let h2 = Hook {
+        name: "Hook2".to_string(),
+        description: "Second hook".to_string(),
+        hook: None,
+    };
+    h1.hook = Some(Box::new(&h2));
+    h1.process();
 }
