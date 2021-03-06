d2c0e856cfe3896a61baf19bf9f859d66feb7191
Steven Black <steveb@stevenblack.com>
2021-03-05 21:06:48 -0500

    First crack at a generic parsers package.

---
+++ src/parsers.rs
@@ -0,0 +1,46 @@
+use crate::hook::*;
+
+struct UppercaseParserHook<'a> {
+    pub hook: Option<&'a mut Hook<'a>>,
+    pub hooks: Vec<&'a mut Hook<'a>>,
+}
+
+impl<'a> Hooking<'a> for UppercaseParserHook<'a> {
+    type Thing = String;
+
+    fn sethook(&mut self, _t: &'a mut Self) -> &mut Self {
+        self
+    }
+
+    fn zethook(&mut self, _t: &'a mut Self) -> () {}
+
+    fn preprocess(&mut self, thing: Self::Thing) -> (bool, Self::Thing) {
+        (true, thing)
+    }
+    fn process(&mut self, thing: Self::Thing) -> Self::Thing {
+        self.execute(thing)
+    }
+    fn execute(&mut self, thing: Self::Thing) -> Self::Thing {
+        thing.to_uppercase()
+    }
+    fn postprocess(&mut self, thing: Self::Thing) -> Self::Thing {
+        thing
+    }
+}
+
+struct ItalicBoldParserHook;
+struct TagCloseParserHook;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn uppercase_parser_hook_test() {
+        // assert_eq!(2 + 2, 4);
+        let mut h = UppercaseParserHook {
+            hook: None,
+            hooks: Vec::new(),
+        };
+        assert_eq!(h.process("hello".to_string()), "HELLO".to_string());
+    }
+}
