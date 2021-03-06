28645d6b016c1ac4f1c14a11ba841f6636d82f3f
Steven Black <steveb@stevenblack.com>
2021-03-05 22:37:41 -0500

    Naïve implementation of UppercaseParserHook.

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,9 +1,22 @@
 use crate::hook::*;
 
+/// A simple hook.  Feed it a String and it returns it transformed to uppercase
 struct UppercaseParserHook<'a> {
+    pub name: String,
     pub hook: Option<&'a mut Hook<'a>>,
     pub hooks: Vec<&'a mut Hook<'a>>,
 }
+
+impl<'a> Default for UppercaseParserHook<'a> {
+    fn default() -> UppercaseParserHook<'a> {
+        UppercaseParserHook {
+            name: "".to_string(),
+            hook: None,
+            hooks: Vec::new(),
+        }
+    }
+}
+
 
 impl<'a> Hooking<'a> for UppercaseParserHook<'a> {
     type Thing = String;
@@ -17,19 +30,19 @@ impl<'a> Hooking<'a> for UppercaseParserHook<'a> {
     fn preprocess(&mut self, thing: Self::Thing) -> (bool, Self::Thing) {
         (true, thing)
     }
+
     fn process(&mut self, thing: Self::Thing) -> Self::Thing {
         self.execute(thing)
     }
+
     fn execute(&mut self, thing: Self::Thing) -> Self::Thing {
         thing.to_uppercase()
     }
+
     fn postprocess(&mut self, thing: Self::Thing) -> Self::Thing {
         thing
     }
 }
-
-struct ItalicBoldParserHook;
-struct TagCloseParserHook;
 
 #[cfg(test)]
 mod tests {
@@ -38,8 +51,7 @@ mod tests {
     fn uppercase_parser_hook_test() {
         // assert_eq!(2 + 2, 4);
         let mut h = UppercaseParserHook {
-            hook: None,
-            hooks: Vec::new(),
+            ..Default::default()
         };
         assert_eq!(h.process("hello".to_string()), "HELLO".to_string());
     }
